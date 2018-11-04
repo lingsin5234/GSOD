fa36cb3558f1bdbefe713fdc1ce5991c9db372da
lingsin5234 <sinto.005234@gmail.com>
2018-11-04 10:42:09 -0500

    log completion to Jobs table

--- GSOD_load.py
+++ GSOD_load.py
@@ -9,20 +9,20 @@ engine = create_engine("mysql+mysqldb://rasppi:"+'Apr!lf00L$'+"@192.168.1.215/te
 
 # set directory
 print("Changing directories...")
-# os.chdir("../../Personal/Practice/GSOD") # work laptop
+os.chdir("../../Personal/Practice/GSOD") # work laptop
 os.chdir("/home/pi/z_extract/GSOD_daily")
-
-# connect to MySQL -- change to not root in future...
-# cnx = msq.connect(user='rasppi', passwd='Apr!lf00L$',
-#                   host='192.168.1.215', port=3306, db='test')
 
 # get csv data and send
 df = pd.read_csv("gsod_2018_final.csv")
-# df.to_sql(con=cnx, name='gsod2018', if_exists='append', flavor='mysql', index=False)
 logf = open("download.log", "w")
 try:
 	df.to_sql(con=engine, name='gsod2018', if_exists='append', index=False)
 except Exception as e:
 	logf.write('%s' % e)
-# cnx.commit()
+# write job as success for now - no way to catch errors sooner.
+finally:
+	j = {'job_nm': ['GSOD_ETL'], 'status': ['Succeeded']} # 'finish_dt': now.strftime("%Y-%m-%d %H:%M:%S")}
+	jlog = pd.DataFrame(data=j)
+	jlog.to_sql(con=engine, name='Jobs', if_exists='append', index=False)
+
 print ("Upload completed.")
