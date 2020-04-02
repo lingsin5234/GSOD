a8a18ddc17c2435d46bfbc9bdc28e006e9f6c546
lingsin5234 <sinto.005234@gmail.com>
2020-04-02 11:44:20 -0600

    onlygsod app branch merged

---
+++ views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from djangoapps.utils import get_this_template
+
+
+# homepage
+def homepage(request):
+
+    return render(request, 'pages/gsod_home.html')
+
+
+# project page
+def project_markdown(request):
+
+    page_height = 1050
+    f = open('baseball/README.md', 'r')
+    if f.mode == 'r':
+        readme = f.read()
+        page_height = len(readme)/2 + 200
+
+    content = {
+        'readme': readme,
+        'page_height': page_height
+    }
+
+    template_page = get_this_template('baseball', 'project.html')
+
+    return render(request, template_page, content)
