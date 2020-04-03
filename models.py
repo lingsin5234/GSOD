11afd831bacb125e2d50eb616c2ff36e72890dd8
lingsin5234 <sinto.005234@gmail.com>
2020-04-03 14:29:37 -0600

    Station model set up

--- models.py
+++ models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+
+    id = models.CharField(max_length=30, primary_key=True)
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    elevation = models.FloatField()
+    elevation_unit = models.CharField(max_length=15)
+    data_coverage = models.FloatField()
+    min_date = models.DateField()
+    max_date = models.DateField()
+
+    def __init__(self):
+        return self.id + ' - ' + self.name
