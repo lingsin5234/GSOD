5b08d589e727fd9d1d485a07080c936f518a0d53
lingsin5234 <sinto.005234@gmail.com>
2020-04-04 13:27:40 -0600

    batch add all canadian stations

--- functions.py
+++ functions.py
@@ -3,17 +3,25 @@ from .oper import ghcnd_extract as ge
 import json
 from itertools import islice
 from .models import Station
+from time import sleep
 
 
 def test_run():
     # get list of stations
-    results = ge.get_stations(['CA'], 'FIPS')
+    results = ge.get_stations(['CA'], 'FIPS', 0)
     results = json.loads(results)
     stations = results['results']
+    count = results['metadata']['resultset']['count'] // 1000 + 1
 
     # setup objects for importing the Stations
-    for i in stations:
-        y = Station(**i)
-        y.save()
+    for c in range(count):
+        print(c)
+        results = ge.get_stations(['CA'], 'FIPS', c * 1000)
+        results = json.loads(results)
+        stations = results['results']
+        for i in stations:
+            y = Station(**i)
+            y.save()
+        sleep(0.5)
 
     return True
