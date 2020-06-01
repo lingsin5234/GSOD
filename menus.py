f584365815866d3d81b063563c4f285c98482bf7
lingsin5234 <sinto.005234@gmail.com>
2020-06-01 11:40:18 -0600

    gsod ready for prod

--- menus.py
+++ menus.py
@@ -6,5 +6,4 @@ from . import views as gsod_vw
 # add items to the menu
 Menu.add_item("noaa", MenuItem("My Portfolio", url="/", weight=10))
 Menu.add_item("noaa", MenuItem("NOAA Project", reverse(gsod_vw.project_markdown), weight=10))
-Menu.add_item("noaa", MenuItem("Stations", reverse(gsod_vw.list_stations), weight=10))
-Menu.add_item("noaa", MenuItem("Map of Edmonton", reverse(gsod_vw.map_test), weight=10))
+Menu.add_item("noaa", MenuItem("US Hist. Weather Map", reverse(gsod_vw.map_test), weight=10))
