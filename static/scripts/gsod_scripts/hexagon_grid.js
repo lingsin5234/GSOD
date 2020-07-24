1da257b93efe629e11f5ee550a8bd35ada5cbc8e
lingsin5234 <sinto.005234@gmail.com>
2020-07-24 14:27:59 -0600

    added colours and stroke (lines)

--- static/scripts/gsod_scripts/hexagon_grid.js
+++ static/scripts/gsod_scripts/hexagon_grid.js
@@ -10,24 +10,24 @@ function single_hexagon(radius, long, lat) {
     // L\ triangle (see notes)
     // w (bottom) = r * cos 60 = r * 1/2
     // h (left) = r * sin 60 = r * sqrt(3)/2
-    width_factor = 1/2 * radius
-    height_factor = Math.sqrt(3) / 2 * radius
+    width_factor = 1/2 * radius;
+    height_factor = Math.sqrt(3) / 2 * radius;
 
     // use the CENTER as reference point; ASSUME NORTHERN HEMISPHERE FOR NOW
     // x,y is [long,lat] format
-    left = [long - radius, lat]
-    right = [long + radius, lat]
-    top_left = [long - width_factor, lat - height_factor]
-    top_right = [long + width_factor, lat - height_factor]
-    bot_left = [long - width_factor, lat + height_factor]
-    bot_right = [long + width_factor, lat + height_factor]
+    left = [long - radius, lat];
+    right = [long + radius, lat];
+    top_left = [long - width_factor, lat - height_factor];
+    top_right = [long + width_factor, lat - height_factor];
+    bot_left = [long - width_factor, lat + height_factor];
+    bot_right = [long + width_factor, lat + height_factor];
 
-    return [left, right, top_left, top_right, bot_left, bot_right]
+    return [left, right, top_left, top_right, bot_left, bot_right];
 }
 
 
 // draw hexagon
-function draw_hexagon(container, left, right, top_left, top_right, bot_left, bot_right) {
+function draw_hexagon(container, left, right, top_left, top_right, bot_left, bot_right, colour) {
 
     var canvas = document.getElementById(container);
     if (canvas.getContext) {
@@ -42,16 +42,19 @@ function draw_hexagon(container, left, right, top_left, top_right, bot_left, bot
         ctx.lineTo(bot_right[0], bot_right[1]);
         ctx.lineTo(bot_left[0], bot_left[1]);
         ctx.lineTo(left[0], left[1]);
+        ctx.strokeStyle = "#000000";
+        ctx.stroke();
+        ctx.fillStyle = colour;
         ctx.fill();
     }
     return true;
 }
 
 
-// generate start points based on the radius
+// generate start points based on the radius; offsets are actually the top-left-most [long, lat] coordinates
 function create_start_points(radius, rows, columns, left_offset, top_offset) {
 
-    points = []
+    points = [];
     // start with rows
     for (var r=0; r < rows; r++) {
 
@@ -71,5 +74,14 @@ function create_start_points(radius, rows, columns, left_offset, top_offset) {
         }
     }
 
-    return points
+    return points;
+}
+
+
+// calculate the desired radius based on the top-left-most [long, lat] coordinates and desired measured radius (in km)
+function calculate_radius(long, lat, measured_radius) {
+
+    // using the longitude, calculate the measured radius in EAST-WEST direction
+
+    return radius;
 }
