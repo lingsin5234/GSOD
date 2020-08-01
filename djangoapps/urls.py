3ebc40b3ead5d5e1a327b43166ea1176dffeee0d
lingsin5234 <sinto.005234@gmail.com>
2020-07-31 19:53:46 -0600

    comment out the project markdown for now

--- djangoapps/urls.py
+++ djangoapps/urls.py
@@ -21,7 +21,7 @@ from gsod import views as gsod_vw
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'^$', gsod_vw.homepage),
-    re_path(r'^project/', gsod_vw.project_markdown),
+    # re_path(r'^project/', gsod_vw.project_markdown),
     re_path(r'^stations/', gsod_vw.list_stations),
     re_path(r'^usa-map/', gsod_vw.map_test),
     re_path(r'^mapbox', gsod_vw.map_box_test, name='mapbox'),
--- gsod/views.py
+++ gsod/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from djangoapps.utils import get_this_template
+# from djangoapps.utils import get_this_template
 import os
 import json
 from .models import Station, GHCND
@@ -125,6 +125,7 @@ def homepage(request):
     return render(request, 'pages/gsod_home.html')
 
 
+'''
 # project page
 def project_markdown(request):
 
@@ -142,6 +143,7 @@ def project_markdown(request):
     template_page = get_this_template('gsod', 'project.html')
 
     return render(request, template_page, content)
+'''
 
 
 # stations list
