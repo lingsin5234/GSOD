d0980a0e754c1607dee2694a6f8d7b199712e139
lingsin5234 <sinto.005234@gmail.com>
2020-07-31 19:45:06 -0600

    testing python job for running calculation

--- gsod/jobs/daily/calculate_hexGrid.py
+++ gsod/jobs/daily/calculate_hexGrid.py
@@ -1,7 +1,6 @@
 # CALCULATE THE HEX GRID
 from django_extensions.management.jobs import DailyJob
-import requests
-import json
+from requests_html import HTMLSession
 
 
 # this is a weekly job that loads GHCND data for all station
@@ -10,6 +9,12 @@ class Job(DailyJob):
 
     def execute(self):
 
-        #
+        # request
+        session = HTMLSession()
+
+        r = session.get('http://127.0.0.1:8000/calculate-hexGrid/2020-05-11/')
+
+        c = r.html.render()  # this call executes the js in the page
+        print(c)
 
         return True
